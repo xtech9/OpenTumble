8c4588ae2f554581dde5f750f910df0d94539aa0
Jacob Prystowsky <jprystowsky@ohio-cpa.com>
2013-06-24 19:38:29 -0400

    Updating tests.

---
+++ test/io/mapping/apps/opentumble/ProdNetKeyNetworkFactoryTest.java
@@ -0,0 +1,36 @@
+package io.mapping.apps.opentumble;
+
+import com.google.bitcoin.core.ECKey;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
+
+public class ProdNetKeyNetworkFactoryTest {
+	private KeyNetworkFactory mKeyNetworkFactory;
+
+	@Before
+	public void setUp() throws Exception {
+		mKeyNetworkFactory = new ProdNetKeyNetworkFactory();
+		assertNotNull(mKeyNetworkFactory);
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testCreateKeyNullCreator() {
+		mKeyNetworkFactory.createKey();
+	}
+
+	@Test
+	public void testCreateKey() throws Exception {
+		mKeyNetworkFactory.setEcKeyCreator(new EcKeyCreator() {
+			@Override
+			public ECKey createEcKey() {
+				return new ECKey();
+			}
+		});
+		assertNotNull(mKeyNetworkFactory.getEcKeyCreator());
+
+		mKeyNetworkFactory.createKey();
+		assertNotNull(mKeyNetworkFactory.getEcKey());
+	}
+}
